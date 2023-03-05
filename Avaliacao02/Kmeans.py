01be450ce8a1640721c5926cc9d9a93855c50848
Davi Nascimento <davinascimento19072001@gmail.com>
2023-03-05 17:10:05 -0300

    Finished the first step

--- Avaliacao02/Kmeans.py
+++ Avaliacao02/Kmeans.py
@@ -1,37 +1,114 @@
 from math import *
-def distanciaEuclidiana(ponto, centroide):
-    quant = len(ponto)
-    distancia = 0
-    for i in range(0, quant):
-        distancia += pow((ponto[i] - centroide[i]), 2)
-    distancia = sqrt(distancia)
-    return distancia
+from random import randint
 
-def mostClose(x, y):
-    array = [[], []]
-    for i in x:
-        dis = 9999999
-        cont =0
-        posi = 0
-        for j in y:
-            if (distanciaEuclidiana(i, j) < dis):
-                dis = distanciaEuclidiana(i, j)
-                posi = cont
-            cont += 1
-        array[posi].append(i)
-    print(array)
+class Kmeans:
+    def __init__(self, n_clusters):
+        self.numClusters = n_clusters
+        self.clusters = []
+        self.centroides = []
+        self.base = []
+        self.anterior = []
 
+    #Calcula a distância entre dois pontos de um plano 
+    def distanciaEuclidiana(self, ponto, centroide):
+        quant = len(ponto)
+        distancia = 0
+        for i in range(0, quant):
+            distancia += pow((ponto[i] - centroide[i]), 2)
+        distancia = sqrt(distancia)
+        return distancia
+    
+    #Pega uma base de dados(pontos do plano) e escolhe de acordo com a quantidade de 
+    # clusters quais serão os centroides
+    def selectCentroides(self, x):
+        quantEle = len(x)
+        preenche = 0
+        posiEle = []
+        while(True):
+            num = randint(0, quantEle-1)
+            if (num not in posiEle):
+                posiEle.append(num)
+                preenche += 1
+            if (preenche == self.numClusters):
+                break
+        y = []
+        for i in range(0, self.numClusters):
+            y.append(x[posiEle[i]])
+        self.centroides = y
+
+    #Pega a base de dados(pontos do plano) e os centroides e coloca em seus respectivos clusters
+    def separador(self, x):
+        self.base = x
+        self.selectCentroides(x)
+        array = []
+        for i in range(0, self.numClusters):
+            array.append([])
+        for i in x:
+            dis = 9999999
+            cont =0
+            posi = 0
+            for j in self.centroides:
+                if (self.distanciaEuclidiana(i, j) < dis):
+                    dis = self.distanciaEuclidiana(i, j)
+                    posi = cont
+                cont += 1
+            array[posi].append(i)
+        self.clusters = array
+        self.anterior = array
+
+    #Pega os novos centroides e calcula os novos clusters
+    def newSeparador(self):
+        array = []
+        for i in range(0, self.numClusters):
+            array.append([])
+        for i in self.base:
+            dis = 9999999
+            cont =0
+            posi = 0
+            for j in self.centroides:
+                if (self.distanciaEuclidiana(i, j) < dis):
+                    dis = self.distanciaEuclidiana(i, j)
+                    posi = cont
+                cont += 1
+            array[posi].append(i)
+        self.clusters = array
+        self.newCentroides()
+    
+    #Pegar os clusters e calcular os novos centroides
+    def newCentroides(self):
+        centroides = []
+        for i in self.clusters:
+            aux = []
+            for j in range(0, len(i[0])):
+                cont = 0
+                for k in i:
+                    cont += k[j]
+                cont = cont / len(i)
+                aux.append(cont)
+            centroides.append(aux)
+        self.centroides = centroides
+        if self.anterior == self.centroides:
+            return self.centroides
+        else: 
+            self.anterior = self.centroides
+            self.newSeparador()
+    
+    #Get centroides
+    def getCentroides(self):
+        return self.centroides
+    
+    #Get clusters
+    def getClusters(self):
+        return self.clusters
 
 x = [[1, 4], [2, 6], [1, 9], [2, 8], [3, 4]]
-y = [[3, 2], [4, 6]]
-for i in range(0, 5):
-    print(distanciaEuclidiana(x[i], y[0]))
-print("#########################")
-for i in range(0, 5):
-    print(distanciaEuclidiana(x[i], y[1]))
-print("#########################")
-mostClose(x, y)
-#distanciaEuclidiana(x, y)
+teste =  Kmeans(2)
+teste.separador(x)
+print(teste.getCentroides())
+print(teste.getClusters())
+teste.newCentroides()
+print(teste.getCentroides())
+print(teste.getClusters())
 
 
 
